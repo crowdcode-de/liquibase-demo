39d7c10e098a618faf4c7d3ed6fdac7b07fabd5b
Marcus Noerder-Tuitje <marcus.noerder-tuitje@crowdcode.io>
2026-03-12 10:02:53 +0100

    Latest Version

---
+++ src/main/java/io/crowdcode/training/liquibase/LiquibaseDemoApplication.java
@@ -0,0 +1,13 @@
+package io.crowdcode.training.liquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibaseDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/crowdcode/training/liquibase/LiquibaseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.crowdcode.training.liquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
